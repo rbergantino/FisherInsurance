975e7347b87605d58419d038e31251f0d9e1f86a
Ross Bergantino <Ross Bergantino>
2017-02-01 18:13:51 -0500

    Controllers added - auto, life, home, customercare

---
+++ controllers/HomeInsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+public class HomeInsController : Controller
+{
+ public IActionResult Index()
+ {
+ return Ok("This is the index of the HomeInsController");
+ }
+ public IActionResult Quote()
+ {
+ return Ok("This is the Quotes page of the HomeInsController");
+ }
+}
