a515f6d4e99eb5774782cef194acb775b4136982
Ross Bergantino <Ross Bergantino>
2017-02-02 01:00:08 -0500

    updated controllers because HomeIns looked dumb

---
+++ controllers/HomeownersController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+public class HomeownersController : Controller
+{
+ public IActionResult Index()
+ {
+ return Ok("This is the index of the HomeownersController");
+ }
+ public IActionResult Quote()
+ {
+ return Ok("This is the Quotes page of the HomeownersController");
+ }
+}
